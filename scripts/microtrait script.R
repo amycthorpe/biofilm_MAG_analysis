c9cd8b51fcde8aa3982484224b751ee61d08addd
Amy Thorpe <151149097+amycthorpe@users.noreply.github.com>
2025-07-07 14:39:40 +0100

    Update microtrait script.R

--- scripts/microtrait script.R
+++ scripts/microtrait script.R
@@ -3,7 +3,7 @@
 
 
 # load libraries
-library(microtrait)
+# library(microtrait)
 library(readr)
 library(dplyr)
 library(tibble)
@@ -13,6 +13,7 @@ library(ggplot2)
 library(RColorBrewer)
 library(patchwork)
 library(reshape2)
+library(ggnewscale)
 
 set.seed(123)
 
@@ -24,6 +25,21 @@ gtdb <- read.csv("data/checkm_gtdb.csv", row.names=1, check.names=F, sep=",") %>
   select(14:20)
 
 coverage <- read.csv("data/finalbins_coverage.csv", row.names=1, check.names=F, sep=",")
+
+# filter low quality MAGs
+checkm <- read.csv("data/checkm_gtdb.csv", row.names=1, check.names=F, sep=",")
+
+checkm <- mutate(checkm, Completeness_quality = case_when(
+  Completeness >= 90 & Contamination <= 5 ~ "Near-complete MAGs",
+  Completeness >= 70 & Completeness < 90 & Contamination <= 10 ~ "Medium-quality MAGs",
+  TRUE ~ "Low-quality MAGs"
+))
+
+checkm <- subset(checkm, Completeness_quality != "Low-quality MAGs")
+coverage <- coverage[rownames(coverage) %in% rownames(checkm), ]
+gtdb <- gtdb[rownames(gtdb) %in% rownames(checkm), ]
+microtrait <- microtrait[microtrait$id %in% rownames(checkm), ]
+
 
 # convert to relative abundance
 coverage_rel <- coverage %>%
@@ -187,3 +203,177 @@ patchwork
 
 ggsave("output/microtrait_phylum_combined2.png", patchwork, width = 18, height = 8, units = "in", dpi = 600)
 ggsave("output/microtrait_phylum_combined2.pdf", patchwork, width = 200, height = 120, units = "mm", dpi = 600)
+
+
+# genus level
+# deal with unassigned genera
+fill_unassigned <- function(row) {
+  last_known <- NA
+  for (i in seq_along(row)) {
+    if (is.na(row[i]) || row[i] == "") {
+      if (!is.na(last_known)) {
+        row[i] <- paste0("unassigned ", last_known)
+      } else {
+        row[i] <- "unassigned root"
+      }
+    } else {
+      last_known <- row[i]
+    }
+  }
+  return(row)
+}
+
+gtdb2 <- as.data.frame(t(apply(gtdb, 1, fill_unassigned)), stringsAsFactors = FALSE)
+
+# merge
+microtrait_tax2 <- merge(microtrait, gtdb2, by.x="id", by.y="row.names")
+microtrait_merged2 <- merge(microtrait_tax2, coverage_mean, by.x="id", by.y="label")
+
+# calculate weighted genus counts
+traits <- microtrait_merged2 %>% 
+  mutate(across(all_of(trait_list), ~ . * mean_rel_abund)) %>% # count weighted by mean relative abundance
+  group_by(phylum, genus) %>% 
+  summarise(across(all_of(trait_list), sum),) # sum trait presence by genus
+
+# prepare table for plotting
+traits2 <- traits %>% 
+  column_to_rownames(., var="genus") %>% 
+  select(where(~ !is.numeric(.) || sum(.) != 0)) %>% # remove traits that are all zero
+  rownames_to_column(., var = "genus")
+
+traits_melt <- traits2 %>% 
+  melt(.) %>% 
+  filter(variable %in% trait_list) %>% 
+  mutate(value=na_if(value,0)) %>% # zeros to NA for better visualisation
+  separate(variable, into = c("level1", "level2", "level3","level4","level5", "level6"), # separate traits into their levels
+           sep=":",
+           extra="merge",
+           fill="right") #%>% 
+  # mutate(genus=factor(genus, levels=sort(unique(genus), decreasing=T))) %>%  # reverse genus order for plotting
+  # arrange(genus)
+
+# change NA to last non-NA value within the level columns
+level_columns <- grep("^level", names(traits_melt))  # level columns
+
+for (i in 1:nrow(traits_melt)) {
+  traits_melt[i, level_columns] <- replace_na(traits_melt[i, level_columns]) # apply function
+}
+
+# harmonise level labels:
+traits_melt2 <- traits_melt %>% 
+  filter(!(level2 == "Specific" & level4 == "scavenging of reactive oxygen species")) %>% 
+  filter(!(level2 == "Specific" & level5 == "protection, repair, degradation of denatured/misfolded proteins")) %>% 
+  filter(!(level4 == "redox sensing" & level3 == "oxygen limitation")) %>% 
+  mutate(level5 = ifelse(level4 == "redox sensing", paste(level3, level5, sep= " "), level5)) %>% 
+  mutate(level5 = ifelse(level6 == "membrane stabilization", paste(level3, level5, sep = " "), level5)) %>% 
+  mutate(level2 = ifelse(level2 %in% c("General", "Specific"), level1, level2))
+
+# subset to top genera
+coverage_mean_genera <- coverage_rel %>% 
+  t(.) %>% 
+  merge(., gtdb2, by="row.names") %>% 
+  group_by(genus) %>% 
+  summarise(across(where(is.numeric), sum)) %>% 
+  column_to_rownames(., var="genus") %>% 
+  mutate(mean_coverage = rowMeans(.)) %>%
+  arrange(desc(mean_coverage)) %>%
+  slice_head(n = 30)
+
+traits_melt_sub <- traits_melt2[traits_melt2$genus %in% rownames(coverage_mean_genera), ]
+
+genus_order <- traits_melt_sub %>%
+  distinct(genus, phylum) %>%
+  arrange(phylum, genus) %>%
+  pull(genus)
+
+traits_melt_sub$genus <- factor(traits_melt_sub$genus, levels = genus_order)
+
+# plot
+level_4_plots <- lapply(ordered_groups, function(group) {
+  group_data <- traits_melt_sub %>% filter(level2 == group)
+  
+  group_data <- traits_melt_sub %>%
+    filter(level2 == group) %>%
+    mutate(genus = factor(genus, levels = rev(genus_order))) 
+  
+  ggplot(group_data, aes(x = level4, y = genus, fill = value)) +
+    geom_tile() +
+    scale_fill_gradientn(colors = brewer.pal(9, "Blues"),
+                         limits = c(0,0.05),
+                         name = "Weighted count",
+                         na.value="white") +
+    labs(title = group) + 
+    theme_bw() +
+    theme(panel.border = element_rect(colour = "black", fill = NA, size = 0.4),
+          panel.background = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          legend.position="bottom",
+          legend.text = element_text(angle = 45, size = 5, hjust = 1),
+          strip.background = element_blank(),
+          strip.text = element_text(size = 6, colour = "black"),
+          plot.title = element_text(size=6, colour="black", hjust=0.5),
+          axis.title = element_blank(),
+          axis.text.y = element_text(size = 5, colour = "black"),
+          axis.text.x = element_text(size = 5, colour = "black", angle = 45, hjust = 1)) 
+})
+
+level_5_plots <- lapply(ordered_groups, function(group) {
+  group_data <- traits_melt_sub %>% filter(level2 == group)
+  
+  group_data <- traits_melt_sub %>%
+    filter(level2 == group) %>%
+    mutate(genus = factor(genus, levels = rev(genus_order))) 
+  
+  ggplot(group_data, aes(x = level5, y = genus, fill = value)) +
+    geom_tile() +
+    scale_fill_gradientn(colors = brewer.pal(9, "Blues"),
+                         limits = c(0,0.05),
+                         name = "Weighted count",
+                         na.value="white") +
+    labs(title = group) + 
+    theme_bw() +
+    theme(panel.border = element_rect(colour = "black", fill = NA, size = 0.4),
+          panel.background = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          legend.position="bottom",
+          legend.text = element_text(angle = 45, size = 5, hjust = 1),
+          strip.background = element_blank(),
+          strip.text = element_text(size = 6, colour = "black"),
+          plot.title = element_text(size=6, colour="black", hjust=0.5),
+          axis.title = element_blank(),
+          axis.text.y = element_text(size = 5, colour = "black"),
+          axis.text.x = element_text(size = 5, colour = "black", angle = 45, hjust = 1)) 
+})
+
+patchwork <- (level_4_plots[[1]] +
+                level_4_plots[[2]] +
+                level_4_plots[[3]] +
+                level_4_plots[[4]] +
+                level_5_plots[[5]] +
+                level_5_plots[[6]]) +
+  plot_layout(ncol=6, widths=c(7, 11, 37, 6, 5, 17), guides="collect") &
+  theme(legend.position = "bottom")
+
+patchwork
+
+patchwork[[2]] = patchwork[[2]] + theme(axis.text.y = element_blank(),
+                                        axis.ticks.y = element_blank())
+
+patchwork[[3]] = patchwork[[3]] + theme(axis.text.y = element_blank(),
+                                        axis.ticks.y = element_blank())
+
+patchwork[[4]] = patchwork[[4]] + theme(axis.text.y = element_blank(),
+                                        axis.ticks.y = element_blank())
+
+patchwork[[5]] = patchwork[[5]] + theme(axis.text.y = element_blank(),
+                                        axis.ticks.y = element_blank())
+
+patchwork[[6]] = patchwork[[6]] + theme(axis.text.y = element_blank(),
+                                        axis.ticks.y = element_blank())
+
+patchwork
+
+ggsave("output/microtrait_genus_combined2.png", patchwork, width = 18, height = 8, units = "in", dpi = 600)
+ggsave("output/microtrait_genus_combined2.pdf", patchwork, width = 220, height = 140, units = "mm", dpi = 600)
